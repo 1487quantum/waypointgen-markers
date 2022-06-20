e8e579a45ecc0f6774506485ecc0404c35ad5e0a
1487quantum <1487quantum@users.noreply.github.com>
2022-06-21 00:15:06 +0800

    Convert setpoint_marker to class
    Set intial angle to 0 for markers
    Add debug constructor
    Update package xml

---
+++ include/waypointgen/setpoint_marker.h
@@ -0,0 +1,96 @@
+
+#include <ros/package.h>
+#include <ros/ros.h>
+
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/PoseWithCovariance.h>
+#include <std_msgs/String.h>
+#include <tf/tf.h>
+#include <tf/transform_datatypes.h>
+
+#include <interactive_markers/interactive_marker_server.h>
+#include <interactive_markers/menu_handler.h>
+#include <visualization_msgs/InteractiveMarkerInit.h>
+
+#include <boost/bind.hpp>
+#include <ctime>
+#include <filesystem>
+#include <fstream>
+#include <map>
+#include <yaml-cpp/yaml.h>
+
+#include "waypointgen/waypointgen_utils.h"
+
+typedef std::map<std::string, geometry_msgs::PoseWithCovariance> p_map;
+
+class waypointgen_marker {
+public:
+  waypointgen_marker(const ros::NodeHandle &nh);
+  waypointgen_marker(const ros::NodeHandle &nh, const bool &debug);
+
+  void updateWypt(p_map &tmp, const std::string &wp_name,
+                  const geometry_msgs::PoseWithCovariance &pt);
+  void addWaypointMarker(const unsigned int &mrk_id);
+
+  // Sub/Pub
+  void updateWaypointPos(
+      const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback);
+
+  void setpointListCallback(
+      const visualization_msgs::InteractiveMarkerInitConstPtr msg);
+
+  // Menu
+  void mnu_getLocation(
+      const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback);
+  void mnu_addNewWaypoint(
+      const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback);
+  void mnu_createList(
+      const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback);
+  void mnu_removeWaypoint(
+      const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback);
+  void addContextMnu(const bool &init);
+
+  // Markers
+  void setHeader(visualization_msgs::InteractiveMarker &imk_h,
+                 const int &index_h);
+  void setPos(visualization_msgs::InteractiveMarker &imk_h, const int &sx,
+              const int &sy);
+  visualization_msgs::Marker makeArrow(unsigned char mrk_id);
+
+  // Controls
+  void addIntCtrl(visualization_msgs::InteractiveMarkerControl &imc,
+                  const unsigned char &mk_id);
+  void addMovementControl(visualization_msgs::InteractiveMarkerControl &im_c,
+                          const bool &mw, const bool &mx, const bool &my,
+                          const bool &mz, const std::string &mName,
+                          const bool &mvAxis);
+  void addMotionControl(visualization_msgs::InteractiveMarker &i_mk,
+                        visualization_msgs::InteractiveMarkerControl &imc_am);
+
+  void updateWPList(p_map &tmp);
+
+  void reset_server();
+
+  p_map wpl;
+
+  // Subscriber
+  ros::Subscriber sub_setpoint_list;
+
+  // Menu
+  interactive_markers::MenuHandler menu_handler;
+  waypointgen_utils wpg_utils;
+
+private:
+  bool menu_init_;
+
+  // Marker
+  int marker_id_;
+  int marker_count_; // Keep track of number of waypoints created
+
+  float lastMarker[2] = {0.0, 0.0}; // Get pos of last marker
+
+  bool DEBUG;
+
+  ros::NodeHandle nh_;
+  std::shared_ptr<interactive_markers::InteractiveMarkerServer> server_;
+};
--- src/setpoint_marker.cpp
+++ src/setpoint_marker.cpp
@@ -1,85 +1,28 @@
-#include <ros/ros.h>
-#include <ros/package.h>
+#include "waypointgen/setpoint_marker.h"
 
-#include <geometry_msgs/Point.h>
-#include <geometry_msgs/PoseWithCovariance.h>
-#include <std_msgs/String.h>
-#include <tf/tf.h>
-#include <tf/transform_datatypes.h>
+waypointgen_marker::waypointgen_marker(const ros::NodeHandle &nh) {
+  waypointgen_marker(nh, false);
+}
 
-#include <interactive_markers/interactive_marker_server.h>
-#include <interactive_markers/menu_handler.h>
-#include <visualization_msgs/InteractiveMarkerInit.h>
+waypointgen_marker::waypointgen_marker(const ros::NodeHandle &nh,
+                                       const bool &debug)
+    : menu_init_(false), marker_id_(0), marker_count_(0), DEBUG(debug) {
 
-#include <ctime>
-#include <filesystem>
-#include <fstream>
-#include <map>
-#include <yaml-cpp/yaml.h>
+  this->nh_ = nh;
 
-#include "waypointgen/waypointgen_utils.h"
+  server_.reset(new interactive_markers::InteractiveMarkerServer(
+      "setpoint_marker", "", false));
 
-#define DEBUG 1
+  // Add waypoint at start
+  addWaypointMarker(this->marker_id_);
 
-typedef std::map<std::string, geometry_msgs::PoseWithCovariance> p_map;
+  // Subscribe to the waypoints published
+  sub_setpoint_list =
+      this->nh_.subscribe("setpoint_marker/update_full", 1,
+                          &waypointgen_marker::setpointListCallback, this);
+}
 
-std::shared_ptr<interactive_markers::InteractiveMarkerServer> server;
-
-// Marker
-unsigned char marker_id = 0;    // Take a max value of 255 waypoints
-unsigned char marker_count = 0; // Keep track of number of waypoints created
-
-p_map wpl;
-
-float lastMarker[2] = {0.0, 0.0}; // Get pos of last marker
-
-// Subscriber
-ros::Subscriber sub_setpoint_list;
-
-// Menu
-interactive_markers::MenuHandler menu_handler;
-auto menuInit{false};
-
-waypointgen_utils wpg_utils;
-
-void updateWypt(p_map &tmp, const std::string &wp_name,
-                const geometry_msgs::PoseWithCovariance &pt);
-void addWaypointMarker(const unsigned int &mrk_id);
-
-// Sub/Pub
-void updateWaypointPos(
-    const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback);
-
-void setpointListCallback(
-    const visualization_msgs::InteractiveMarkerInitConstPtr msg);
-// Menu
-void mnu_addNewWaypoint(
-    const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback);
-void mnu_createList(
-    const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback);
-void mnu_removeWaypoint(
-    const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback);
-void addContextMnu(const bool &init);
-
-// Markers
-void setHeader(visualization_msgs::InteractiveMarker &imk_h,
-               const int &index_h);
-void setPos(visualization_msgs::InteractiveMarker &imk_h, const int &sx,
-            const int &sy);
-visualization_msgs::Marker makeArrow(unsigned char mrk_id);
-// Controls
-void addIntCtrl(visualization_msgs::InteractiveMarkerControl &imc,
-                const unsigned char &mk_id);
-void addMovementControl(visualization_msgs::InteractiveMarkerControl &im_c,
-                        const bool &mw, const bool &mx, const bool &my,
-                        const bool &mz, const std::string &mName,
-                        const bool &mvAxis);
-void addMotionControl(visualization_msgs::InteractiveMarker &i_mk,
-                      visualization_msgs::InteractiveMarkerControl &imc_am);
-
-void updateWPList(p_map &tmp);
-
-void updateWaypointPos(
+void waypointgen_marker::updateWaypointPos(
     const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback) {
 
   if (feedback->event_type ==
@@ -111,12 +54,12 @@ void updateWaypointPos(
         p_quat); // Update pose in map, geometry_msgs::PoseWithCovariance
 
     updateWypt(wpl, feedback->marker_name, pwc);
-    server->applyChanges();
+    server_->applyChanges();
   }
 }
 
 // Subscriber callback
-void setpointListCallback(
+void waypointgen_marker::setpointListCallback(
     const visualization_msgs::InteractiveMarkerInitConstPtr msg) {
   geometry_msgs::PoseWithCovariance pt;
   updateWPList(wpl);
@@ -131,13 +74,13 @@ void setpointListCallback(
         mk.pose.orientation
             .w); // Create Quaternion for rotation, tf::Quaternion
     pt = wpg_utils.addPoseCov(p_point, qtmp); // Create pose
-    updateWypt(wpl, mk.name, pt);   // Add to map
+    updateWypt(wpl, mk.name, pt);             // Add to map
     if (DEBUG)
       wpg_utils.printDebugPose("LIST SUB CALLBACK", mk.name, pt);
   }
 }
 
-void updateWPList(p_map &tmp) {
+void waypointgen_marker::updateWPList(p_map &tmp) {
   if (tmp.empty())
     ROS_INFO("Map is empty! Updating...");
 
@@ -146,8 +89,9 @@ void updateWPList(p_map &tmp) {
 }
 
 // Add waypoint to map
-void updateWypt(p_map &tmp, const std::string &wp_name,
-                const geometry_msgs::PoseWithCovariance &pt) {
+void waypointgen_marker::updateWypt(
+    p_map &tmp, const std::string &wp_name,
+    const geometry_msgs::PoseWithCovariance &pt) {
   if (tmp.insert(std::make_pair(wp_name, pt)).second == false) {
     ROS_WARN("Overwriting %s", wp_name.c_str());
     tmp[wp_name] = pt;
@@ -159,7 +103,7 @@ void updateWypt(p_map &tmp, const std::string &wp_name,
 }
 
 // Show selected waypoint Location
-void mnu_getLocation(
+void waypointgen_marker::mnu_getLocation(
     const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback) {
   geometry_msgs::PoseWithCovariance pwc_gl;
   wpg_utils.set_pose_position(pwc_gl.pose.position, feedback->pose.position);
@@ -173,10 +117,10 @@ void mnu_getLocation(
 }
 
 // Add waypoint
-void mnu_addNewWaypoint(
+void waypointgen_marker::mnu_addNewWaypoint(
     const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback) {
-  if (marker_id < 256) {
-    addWaypointMarker(++marker_id);
+  if (marker_id_ < 256) {
+    addWaypointMarker(++marker_id_);
     ROS_INFO("Waypoint Added!");
   } else {
     ROS_INFO("Setpoint limit reached!");
@@ -184,7 +128,7 @@ void mnu_addNewWaypoint(
 }
 
 // Remove waypoint_
-void mnu_removeWaypoint(
+void waypointgen_marker::mnu_removeWaypoint(
     const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback) {
   std_msgs::String dispMsg;               // To display on console
   auto markerName{feedback->marker_name}; // Marker Name, std::string
@@ -195,15 +139,15 @@ void mnu_removeWaypoint(
                              -1))}; // Get last part of string, since name is
                                     // waypoint_[ID]; Convert str->int
 
-  if (marker_count > 1) {
+  if (marker_count_ > 1) {
     wpl.erase(markerName); // Remove from map
 
     // Remove from server
-    server->erase(feedback->marker_name);
-    server->applyChanges();
+    server_->erase(feedback->marker_name);
+    server_->applyChanges();
 
     // Decrement counter
-    marker_count -= 1;
+    --marker_count_;
 
     dispMsg.data = feedback->marker_name;
     dispMsg.data += " removed!";
@@ -214,14 +158,13 @@ void mnu_removeWaypoint(
   ROS_INFO("%s", dispMsg.data.c_str());
 }
 
-void mnu_createList(
+void waypointgen_marker::mnu_createList(
     const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback) {
-  ROS_INFO("%s %i", "[Generating list] Total Waypoints ->", marker_count);
+  ROS_INFO("%s %i", "[Generating list] Total Waypoints ->", marker_count_);
 
   // Save as YAML file
   YAML::Node pts;
-  pts["count"] =
-      static_cast<int>(marker_count); // Write total number of waypoints
+  pts["count"] = marker_count_; // Write total number of waypoints
 
   auto idx{0};
 
@@ -253,7 +196,7 @@ void mnu_createList(
     ROS_INFO("'list' directory not found, creating one -> %s", loc.c_str());
 
   auto lstName = wpg_utils.getCurrentTime() + "_wplist.yaml"; // File Name
-  loc += lstName;                                   // Append to main path
+  loc += lstName; // Append to main path
   if (DEBUG)
     ROS_INFO("Writing waypoint list to %s", loc.c_str());
 
@@ -268,7 +211,7 @@ Create waypoint
 */
 
 // Create Arrow
-visualization_msgs::Marker makeArrow(unsigned char mrk_id) {
+visualization_msgs::Marker waypointgen_marker::makeArrow(unsigned char mrk_id) {
   typedef visualization_msgs::Marker vmarker;
   vmarker sp_marker;
   sp_marker.type = vmarker::ARROW;
@@ -289,8 +232,8 @@ MARKER CONTROLS
 */
 
 // Set InteractiveMarker Header
-void setHeader(visualization_msgs::InteractiveMarker &imk_h,
-               const int &index_h) {
+void waypointgen_marker::setHeader(visualization_msgs::InteractiveMarker &imk_h,
+                                   const int &index_h) {
   // Header
   imk_h.header.frame_id = "map"; // Set frame relative to map frame
   imk_h.header.stamp = ros::Time::now();
@@ -302,15 +245,26 @@ void setHeader(visualization_msgs::InteractiveMarker &imk_h,
 }
 
 // Set InteractiveMarker position
-void setPos(visualization_msgs::InteractiveMarker &imk_h, const int &sx,
-            const int &sy) {
+void waypointgen_marker::setPos(visualization_msgs::InteractiveMarker &imk_h,
+                                const int &sx, const int &sy) {
   imk_h.pose.position.x = sx;
   imk_h.pose.position.y = sy;
+
+  // Initialise default angle at 0
+  tf::Quaternion qtn;
+  qtn.setRPY(0, 0, 0);
+  qtn = qtn.normalize();
+
+  geometry_msgs::Quaternion quat_msg;
+  quaternionTFToMsg(qtn, quat_msg);
+
+  imk_h.pose.orientation = quat_msg;
 }
 
 // Add controls
-void addMotionControl(visualization_msgs::InteractiveMarker &i_mk,
-                      visualization_msgs::InteractiveMarkerControl &imc_am) {
+void waypointgen_marker::addMotionControl(
+    visualization_msgs::InteractiveMarker &i_mk,
+    visualization_msgs::InteractiveMarkerControl &imc_am) {
   // Position & Orientation
   // Add the control to the interactive marker
   addMovementControl(imc_am, 1, 1, 0, 0, "move_x",
@@ -328,10 +282,10 @@ void addMotionControl(visualization_msgs::InteractiveMarker &i_mk,
 
 // Add motion controls (To modify waypoint position and orientation)
 // mvAxis: 0->Rotate, 1:Translate;
-void addMovementControl(visualization_msgs::InteractiveMarkerControl &im_c,
-                        const bool &mw, const bool &mx, const bool &my,
-                        const bool &mz, const std::string &mName,
-                        const bool &mvAxis) {
+void waypointgen_marker::addMovementControl(
+    visualization_msgs::InteractiveMarkerControl &im_c, const bool &mw,
+    const bool &mx, const bool &my, const bool &mz, const std::string &mName,
+    const bool &mvAxis) {
   im_c.orientation.w = mw;
   im_c.orientation.x = mx;
   im_c.orientation.y = my;
@@ -344,7 +298,7 @@ void addMovementControl(visualization_msgs::InteractiveMarkerControl &im_c,
 }
 
 // Add menu
-void addContextMnu(const bool &init) {
+void waypointgen_marker::addContextMnu(const bool &init) {
   if (!init) {
     // Create menu at start if it does not exist
     // Entries
@@ -355,25 +309,32 @@ void addContextMnu(const bool &init) {
     interactive_markers::MenuHandler::EntryHandle entry_generateYAML;
 
     // Add to menu
-    entry_showLoc = menu_handler.insert("Get Location", &mnu_getLocation);
+    entry_showLoc = menu_handler.insert(
+        "Get Location",
+        boost::bind(&waypointgen_marker::mnu_getLocation, this, _1));
 
     // Waypoint submenu
     mnu_entry_waypoint = menu_handler.insert("Waypoint");
-    entry_add =
-        menu_handler.insert(mnu_entry_waypoint, "Add", &mnu_addNewWaypoint);
-    entry_add =
-        menu_handler.insert(mnu_entry_waypoint, "Remove", &mnu_removeWaypoint);
+    entry_add = menu_handler.insert(
+        mnu_entry_waypoint, "Add",
+        boost::bind(&waypointgen_marker::mnu_addNewWaypoint, this, _1));
+    entry_add = menu_handler.insert(
+        mnu_entry_waypoint, "Remove",
+        boost::bind(&waypointgen_marker::mnu_removeWaypoint, this, _1));
 
     // Generate list of waypoints
-    entry_generateYAML =
-        menu_handler.insert("Generate Waypoint List", &mnu_createList);
+    entry_generateYAML = menu_handler.insert(
+        "Generate Waypoint List",
+        boost::bind(&waypointgen_marker::mnu_createList, this, _1));
 
-    menuInit = true; // Make it true so that the menu only initializes once
+    this->menu_init_ =
+        true; // Make it true so that the menu only initializes once
   }
 }
 
-void addIntCtrl(visualization_msgs::InteractiveMarkerControl &imc,
-                const unsigned char &mk_id) {
+void waypointgen_marker::addIntCtrl(
+    visualization_msgs::InteractiveMarkerControl &imc,
+    const unsigned char &mk_id) {
   imc.interaction_mode = visualization_msgs::InteractiveMarkerControl::BUTTON;
   imc.always_visible = true;
 
@@ -382,7 +343,7 @@ void addIntCtrl(visualization_msgs::InteractiveMarkerControl &imc,
   imc.markers.push_back(makeArrow(mk_id));
 }
 
-void addWaypointMarker(const unsigned int &mrk_id) {
+void waypointgen_marker::addWaypointMarker(const unsigned int &mrk_id) {
   visualization_msgs::InteractiveMarker mrk;
   visualization_msgs::InteractiveMarkerControl imc_m;
 
@@ -397,43 +358,37 @@ void addWaypointMarker(const unsigned int &mrk_id) {
   for (auto &i : lastMarker)
     i += 1;
 
-  addMotionControl(mrk, imc_m); // Add motion CONTROLS
-  addContextMnu(menuInit);      // Add MENU
-  addIntCtrl(imc_m, mrk_id);    // Add the control to the interactive marker
+  addMotionControl(mrk, imc_m);    // Add motion CONTROLS
+  addContextMnu(this->menu_init_); // Add MENU
+  addIntCtrl(imc_m, mrk_id);       // Add the control to the interactive marker
   mrk.controls.push_back(imc_m);
 
   // Insert into server
-  server->insert(mrk);
-  server->setCallback(mrk.name,
-                      &updateWaypointPos); // Attach callback when user updates
-                                           // the marker position
+  server_->insert(mrk);
+  server_->setCallback(mrk.name,
+                       boost::bind(&waypointgen_marker::updateWaypointPos, this,
+                                   _1)); // Attach callback when user
+                                         // updates the marker position
 
   menu_handler.apply(
-      *server, mrk.name); // Apply to int_marker.name, which is setpoint_marker
+      *server_, mrk.name); // Apply to int_marker.name, which is setpoint_marker
 
   // Increment marker count tracker
-  marker_count += 1;
+  ++marker_count_;
 
   // Update map_server
-  server->applyChanges();
+  server_->applyChanges();
 }
+
+void waypointgen_marker::reset_server() { server_.reset(); }
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "setpoint_marker");
 
-  server.reset(new interactive_markers::InteractiveMarkerServer(
-      "setpoint_marker", "", false));
-
-  // Add waypoint at start
-  addWaypointMarker(marker_id);
-
-  // Subscriber
   ros::NodeHandle n;
-  // Subscribe to the waypoints published
-  sub_setpoint_list =
-      n.subscribe("setpoint_marker/update_full", 1, setpointListCallback);
+  waypointgen_marker wpm(n, true);
 
   // start the ROS main loop
   ros::spin();
-  server.reset();
+  wpm.reset_server();
 }
